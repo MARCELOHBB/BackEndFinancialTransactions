310688269040fa5b76007090528dc0fc9e06db45
marcelohbb <marcelo_aut@hotmail.com>
2020-04-14 18:36:59 -0400

    subindo a aplicação

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,18 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +20,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute(
+    { title, value, type }: Request,
+    balance: Balance,
+  ): Transaction {
+    if (type === 'outcome' && value > balance.total) {
+      throw Error('Not a valid balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
